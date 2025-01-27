d57b6e929e7028af2ecd3db8bf1a79f7441a50a4
ragu <raguram1201@gmail.com>
2025-01-27 12:24:57 +0530

    Remove commented logo section from index.html

---
+++ assets/js/particle.js
@@ -0,0 +1,70 @@
+particlesJS("particles-js", {
+    particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#ffffff",
+      },
+      shape: {
+        type: "circle",
+        stroke: {
+          width: 0,
+          color: "#000000",
+        },
+      },
+      opacity: {
+        value: 0.5,
+        anim: {
+          enable: false,
+        },
+      },
+      size: {
+        value: 3,
+        random: true,
+        anim: {
+          enable: false,
+        },
+      },
+      line_linked: {
+        enable: true,
+        distance: 150,
+        color: "#ffffff",
+        opacity: 0.4,
+        width: 1,
+      },
+      move: {
+        enable: true,
+        speed: 5,
+        direction: "none",
+        random: true,
+        out_mode: "bounce",
+      },
+    },
+    interactivity: {
+      detect_on: "canvas",
+      events: {
+        onhover: {
+          enable: true,
+          mode: "repulse",
+        },
+        onclick: {
+          enable: true,
+          mode: "push",
+        },
+      },
+      modes: {
+        repulse: {
+          distance: 200,
+        },
+      },
+    },
+    retina_detect: true,
+  });
+  
+  
+  
